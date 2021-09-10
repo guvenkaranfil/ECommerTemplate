857db8e50ab1bdf4aa09118b31f218ca57a6d737
Güven Karanfil <guvenkaranfil235@gmail.com>
2021-09-10 23:07:44 +0300

    refactor: style Button component based on design

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+import Input from './app/components/Input';
+import Button from './app/components/Button';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.sectionTitle}>Welcome!</Text>
+      <View style={{width: 100}}>
+        <Input
+          placeholder="E-mail"
+          value=""
+          onChangeText={val => console.log('val:', val)}
+        />
+      </View>
+      <View style={{width: 200, marginTop: 30}}>
+        <Input
+          placeholder="E-mail"
+          value=""
+          onChangeText={val => console.log('val:', val)}
+        />
+      </View>
+      <View style={{width: 300, marginTop: 30}}>
+        <Input
+          placeholder="E-mail"
+          value=""
+          onChangeText={val => console.log('val:', val)}
+        />
+      </View>
+      <View style={{width: 300, marginTop: 30}}>
+        <Button label="Sign In" onPress={() => console.log('onPress..')} />
+      </View>
     </SafeAreaView>
   );
 };
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {Text, TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  StyleSheet,
+} from 'react-native';
 
 interface ButtonProps extends TouchableOpacityProps {
   label: string;
@@ -8,8 +13,24 @@ interface ButtonProps extends TouchableOpacityProps {
 
 export default function Button({label, ...rest}: ButtonProps) {
   return (
-    <TouchableOpacity {...rest}>
-      <Text>{label}</Text>
+    <TouchableOpacity style={styles.button} {...rest}>
+      <Text style={styles.label}>{label}</Text>
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000',
+    borderRadius: 6,
+  },
+
+  label: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ffffff',
+  },
+});
